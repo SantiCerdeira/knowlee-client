1c9461bb15953e73a9325a3a16a132a705f2ced7
Santiago Cerdeira <santiagocerdeira808@gmail.com>
2023-08-06 20:09:38 -0300

    fixing authentication

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -20,6 +20,7 @@ const AuthProvider = ({ children }) => {
       });
       if (response.status === 200) {
         setToken(null);
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;'
         callback();
       }
     } catch (error) {
