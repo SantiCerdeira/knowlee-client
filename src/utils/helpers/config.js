9441c88cef43254793ebe0cba89477a3bdc3b4a4
Santiago Cerdeira <santiagocerdeira808@gmail.com>
2023-08-08 12:51:53 -0300

    fixing socket

--- src/utils/helpers/config.js
+++ src/utils/helpers/config.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = "http://localhost:4321";
 export const BASE_URL = "https://knowlee-api.onrender.com";
-export const SOCKET_URL = "http://localhost:4321";
+export const SOCKET_URL = "https://knowlee-api.onrender.com";
+// export const SOCKET_URL = "http://localhost:4321";
 export const NODE_ENV = 'production'
 // export const NODE_ENV = 'development'
